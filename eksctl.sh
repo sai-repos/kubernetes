943e54daf5d45fd498a90d02284e712a234e8424
Sai Krishna <krishna.mds595@gmail.com>
2026-06-09 17:52:30 +0530

    Add eksctl installation script
    This script downloads and installs eksctl, a command-line tool for managing Kubernetes clusters on AWS.

---
+++ eksctl.sh
@@ -0,0 +1,12 @@
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify the checksum if needed
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+eksctl version
